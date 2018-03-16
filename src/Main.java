33d91a58bd1bc44b74342142c5a0a308193734c9
zyec32 <njkmrjpfzw@mail.ru>
2018-03-16 23:14:14 +0300

    Add frame.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args){
-        
+        JFrame frame = new JFrame("Evolution");
+        Drawer drawer = new Drawer();
+        frame.add(drawer);
+        frame.setSize(100, 100);
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 }
