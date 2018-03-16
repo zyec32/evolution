3dada57900df3afbfb3caa634808e36c3f599693
zyec32 <njkmrjpfzw@mail.ru>
2018-03-16 23:20:03 +0300

    Add sizes.

--- src/Drawer.java
+++ src/Drawer.java
@@ -2,9 +2,15 @@ import javax.swing.*;
 import java.awt.*;
 
 public class Drawer extends JPanel {
+    private int sizeW;
+    private int sizeH;
+    private int space;
 
-    Drawer(){
+    Drawer(int sizeW, int sizeH, int space){
         super();
+        this.sizeW = sizeW;
+        this.sizeH = sizeH;
+        this.space = space;
     }
 
     @Override
@@ -14,7 +20,7 @@ public class Drawer extends JPanel {
         g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
                 RenderingHints.VALUE_ANTIALIAS_ON);
 
-        g2d.setPaint(Color.gray);
+        g2d.setPaint(Color.black);
         g2d.fillRect(0, 0, getWidth(), getHeight());
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,19 @@
 import javax.swing.*;
 
 public class Main {
+    private static final int startW = 20;
+    private static final int startH = 40;
+    private static final int sizeW = 15;
+    private static final int sizeH = 15;
+    private static final int space = 1;
+    private static final int countW = 90;
+    private static final int countH = 50;
+
     public static void main(String[] args){
         JFrame frame = new JFrame("Evolution");
-        Drawer drawer = new Drawer();
+        Drawer drawer = new Drawer(sizeW, sizeH, space);
         frame.add(drawer);
-        frame.setSize(100, 100);
+        frame.setSize(startW+countW*(sizeW+ space), startH+countH*(sizeH+ space));
         frame.setVisible(true);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
