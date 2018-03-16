1f61d105134af59816a642b068b3d4d9be24eb7f
zyec32 <njkmrjpfzw@mail.ru>
2018-03-16 23:58:04 +0300

    Add Prim.

---
+++ src/Prim.java
@@ -0,0 +1,13 @@
+import java.awt.*;
+
+public class Prim {
+    private Color color;
+
+    Prim(Color color){
+        this.color = color;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+}
