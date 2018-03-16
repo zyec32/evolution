765c0e91aa0477c8723472ff56e2c57bec316e82
zyec32 <njkmrjpfzw@mail.ru>
2018-03-17 00:27:48 +0300

    Add coord at Prim.

--- src/Prim.java
+++ src/Prim.java
@@ -1,15 +1,33 @@
 import java.awt.*;
 
 public class Prim {
+    private int x;
+    private int y;
     private Color color;
     World myWorld;
 
-    Prim(Color color, World world){
+    Prim(int x, int y, Color color, World world){
+        this.x = x;
+        this.y = y;
         this.color = color;
         myWorld = world;
     }
 
+    public void moveTo(int newX, int newY){
+        myWorld.movePrime(x, y, newX, newY);
+        x = newX;
+        y = newY;
+    }
+
     public Color getColor() {
         return color;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
     }
 }
